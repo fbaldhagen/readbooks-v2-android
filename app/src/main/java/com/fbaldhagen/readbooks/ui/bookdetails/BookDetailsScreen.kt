9837c72f8aeac752092bc3bdedb83c823b7aad93
Fredrik Baldhagen <baldhagen@gmail.com>
2026-02-20 03:27:18 +0100

    feat: improve BookDetailsScreen top app bar apperance and behavior

--- app/src/main/java/com/fbaldhagen/readbooks/ui/bookdetails/BookDetailsScreen.kt
+++ app/src/main/java/com/fbaldhagen/readbooks/ui/bookdetails/BookDetailsScreen.kt
@@ -6,11 +6,15 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.fbaldhagen.readbooks.ui.bookdetails.components.BookDetailsContent
@@ -26,6 +30,7 @@ fun BookDetailsScreen(
     viewModel: BookDetailsViewModel = hiltViewModel()
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
     Scaffold(
         topBar = {
@@ -38,10 +43,15 @@ fun BookDetailsScreen(
                             contentDescription = "Back"
                         )
                     }
-                }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent,
+                    scrolledContainerColor = MaterialTheme.colorScheme.surface
+                ),
+                scrollBehavior = scrollBehavior
             )
         },
-        modifier = modifier
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
     ) { innerPadding ->
         when (val current = state) {
             is BookDetailsUiState.Loading -> LoadingIndicator(
